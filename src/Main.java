d2088031a6d4676b426e67bddcfa551ef5a0a6b6
Christoph Rieger <rieger.chris@googlemail.com>
2019-07-18 18:36:28 +0000

    Tier Aufgabe

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    
+    /**
+     * Vererbung
+     */
+    public static void main(String[] args){
+        // Erzeugen Sie eine Hierarchie von Tieren, in der mindestens folgende Klassen enthalten sind:
+        // Tier, Säugetier, Insekt, Vierbeiner, Zweibeiner, Hund, Känguru, Spinne
+
+    }
+    
+}
