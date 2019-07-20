e64afca5e961ecc6d9129c8196cdc9c748230036
Arakai-dev <rose.patrick@gmx.de>
2019-07-20 12:36:35 +0000

    Passt

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 public class Main {
-    
-    /**
-     * Vererbung
-     */
-    public static void main(String[] args){
-        // Erzeugen Sie eine Hierarchie von Tieren, in der mindestens folgende Klassen enthalten sind:
-        // Tier, Säugetier, Insekt, Vierbeiner, Zweibeiner, Hund, Känguru, Spinne
 
+    public static void main(String[] args){
+        Hund köter = new Hund("Köter");
+        
+        System.out.println(köter.getName());
     }
-    
 }
